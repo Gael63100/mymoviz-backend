9964ed5b2221109e75c7e3baae234ba8ea080966
Gael Adjouale <gadjouale@gmail.com>
2019-10-21 04:32:53 +0200

    cors setup

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ var options = {
       connectTimeoutMS: 30000
     }
   }
-}                
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds239071.mlab.com:39071/mymovizapp', options, function (err) {
+}           
+mongoose.connect('mongodb://user:password@ds239071.mlab.com:39071/mymovizapp', options, function (err) {
   console.log(err)
 })
 
@@ -21,13 +21,13 @@ var MovieModel = mongoose.model('movie', movieSchema)
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({ message: 'route /' })
+  res.json({ message: 'Welcome in mymovizapp' })
 })
 
 // LIRE L'INTEGRALITE DES FILMS 
 // // On fait un JSON.PARSE pour convertir au format JSON le format de retour de l'API.
 router.get('/movie', function (req, res, next) {
-  request('https://api.themoviedb.org/3/discover/movie?<api_key=>&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', function (error, response, body) {
+  request('https://api.themoviedb.org/3/discover/movie?api_key=...&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', function (error, response, body) {
     body = JSON.parse(body)
     res.json(body)
   })
